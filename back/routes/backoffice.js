3c3652003d90271e31dfbb160ba9f884d47533e2
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-15 12:23:04 +0200

    resolve issue about else

--- back/routes/backoffice.js
+++ back/routes/backoffice.js
@@ -5,29 +5,28 @@ const path = require('path')
 
 const connection = require('../database')
 
-
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        const uploadPath = 'public'
-        callback(null, uploadPath)
+      const uploadPath = 'public'
+      callback(null, uploadPath)
     },
     filename: function (req, file, callback) {
-        const d = new Date()
-        const date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
-        const filename = file.originalname.replace(/ /g, '-')
+      const d = new Date()
+      const date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
+      const filename = file.originalname.replace(/ /g, '-')
       const dateFilename = date + '-' + filename
       callback(null, dateFilename)
     }
   })
   
   const upload = multer({
-      storage: storage,
-      fileFilter: function (req, file, callback) {
-          const ext = path.extname(file.originalname)
-          if (ext !== '.jpg') {
-              return callback(new Error('Only jpg images are allowed'))
-            }
-            callback(null, true)
+    storage: storage,
+    fileFilter: function (req, file, callback) {
+      const ext = path.extname(file.originalname)
+      if (ext !== '.jpg') {
+        return callback(new Error('Only jpg images are allowed'))
+      }
+        callback(null, true)
     }
 }).single('file')
 
@@ -40,21 +39,21 @@ router.post('/', (req,res) => {
           return res.status(500).json({ error: 'echec2' })
         }
         else {
-            const formData = req.body
-            const imageData = {
-              ...formData,
-              picture: req.file.filename
-            }
-        }
+          const formData = req.body
+          const imageData = {
+            ...formData,
+            picture: req.file.filename
+          }       
         connection.query('INSERT INTO picture SET ?',
         [imageData],
         (err, results) => {
-            if (err) {
-                return res.status(400).json({ error: 'Failed to upload picture' })
-            } else {
-                return res.status(200).json(results)
-            }
+          if (err) {
+            return res.status(400).json({ error: 'Failed to upload picture' })
+          } else {
+            return res.status(200).json(results)
+          }
         })
+      }
     })
 })
 
