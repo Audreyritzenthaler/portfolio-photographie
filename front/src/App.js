fa30de8720b0d7a6feaa6e576f69792f06af1028
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-24 14:27:36 +0200

    create path

--- front/src/App.js
+++ front/src/App.js
@@ -4,18 +4,20 @@ import { Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import BackOffice from './components/BackOffice'
+import Identification from './components/Identification'
 import Portfolio from './components/Portfolio'
 
 
 function App() {
   return (
     <div className="App">
-      {/* <BackOffice />     
-       <Portfolio /> */}
+       {/* <Portfolio /> */}
 
       <Navbar />
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route path='/backoffice' component={BackOffice}/>
+        <Route path='/identification' component={Identification}/>
       </Switch>
       
     </div>
