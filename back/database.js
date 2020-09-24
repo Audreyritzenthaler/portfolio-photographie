b8f72dbf4da268bbe11bed125a67bedf9c5a2ece
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-24 14:16:43 +0200

    add secret

--- back/database.js
+++ back/database.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
+const secret = process.env.JWT_SECRET
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
