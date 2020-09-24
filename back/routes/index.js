387183f2fd4e99c8ef4dd6be4bdc377ae748f2b0
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-24 14:23:15 +0200

    add routes authentification + register

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,8 @@
 const portfolio = require('./portfolio')
 const backoffice = require('./backoffice')
+const authentification = require('./authentification')
+const register = require('./register')
 
 module.exports = {
-    portfolio, backoffice
+    portfolio, backoffice, authentification, register
 }
